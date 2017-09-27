dd542cd19ab9d5501f2b74239e83f452c8d84a9c
vinyard78 <luck.ballaud@wanadoo.fr>
2017-09-27 22:42:05 +0200

    bugfix

--- src/pages/examens/examens.ts
+++ src/pages/examens/examens.ts
@@ -12,7 +12,14 @@ export class ExamensPage {
 	examens: Examen[];
 
   	constructor(public navCtrl: NavController, public modalCtrl: ModalController) {
-  		this.examens = [];
+  		this.examens = [new Examen("BONHOMME DE GOODENOUGH","()",22,22,2),
+          new Examen("BONHOMME DE GOODENOUGH","()",8,10,3),
+          new Examen("BONHOMME DE GOODENOUGH","()",25,39,0,33,45),
+          new Examen("BONHOMME DE GOODENOUGH","()",23,30,0,27,32),
+          new Examen("BONHOMME DE GOODENOUGH","()",8,18,0,16,23),
+          new Examen("BONHOMME DE GOODENOUGH","()",6.6,12.3,4.4),
+          new Examen("BONHOMME DE GOODENOUGH","()",13,15.87,3.15),
+          new Examen("BONHOMME DE GOODENOUGH","()",29,29,4.2)];
   	}
 
   	add():void {
